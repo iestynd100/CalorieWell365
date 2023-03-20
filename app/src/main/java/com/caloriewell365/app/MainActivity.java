ab14f26c7cac2fa3aeef5f4f17d97bc6215dcda0
iestynd100 <iestynjohn10@gmail.com>
2023-03-20 21:04:05 +0000

    CalorieWell365 V1.0.0

---
+++ app/src/main/java/com/caloriewell365/app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.caloriewell365.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("https://eu.jotform.com/app/230606678029359");
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/caloriewell365/app/MyWebViewClient.java
@@ -0,0 +1,25 @@
+package com.caloriewell365.app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class MyWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        String hostname;
+
+        // YOUR HOSTNAME
+        hostname = "eu.jotform.com/app/230606678029359";
+
+        Uri uri = Uri.parse(url);
+        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
+            return false;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
